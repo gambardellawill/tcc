bdda0c2f284843d2877da8a5174bc47af09cc2a7
William Gambardella <williamcampo@gmail.com>
2016-09-02 09:35:20 -0300

    Implementação comm.Send() e comm.Recv() do MPI
    Média dos canais para matriz de luminância I : (R+G+B)/3

--- api/sobel.py
+++ api/sobel.py
@@ -14,17 +14,35 @@ def load_image(inputFile):
     inputArray = np.asarray(imageRaw)
     return inputArray
 
+def convert_intensity(nArray): //Not efficient
+    pixel =
+
+
 comm = mpi.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-imArray = load_image("earth.jpg")
+imageBuffer = numpy.zeros(1)
 
-print(imArray[...,1].shape)
+if rank == 0:
+    if size < 2:
+        print("Operando em modo singleton")
+    else:
+        message = "Operando em modo paralelo com " + str(size) + " cores em uso"
+        print(message)
 
+    imArray = load_image("earth.jpg")
 
-for iterator in range(0,3):
-    save_image(imArray[...,iterator], "earth" + str(iterator)+ ".jpg")
+    imageDimensions = imArray[...,0].shape
 
+    imageWaveDepth = imArray[0,0,:].shape[0]
+    print(imageWaveDepth)
 
-print(rank)
+    for iterator in range(0,imageWaveDepth):
+        comm.Send(imArray[...,iterator], dest=iterator)
+        comm.Recv(imageBuffer, source=iterator)
+        #save_image(imArray[...,iterator], "earth" + str(iterator)+ ".jpg")
+
+else:
+
+    comm.Recv()
