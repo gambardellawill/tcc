728a83cd27f1ba92ddfd6274fcf2b22787633177
William Gambardella <williamcampo@gmail.com>
2016-09-04 09:16:29 -0300

    Implementar funções singleton para comparação

--- api/sobel.py
+++ api/sobel.py
@@ -14,35 +14,62 @@ def load_image(inputFile):
     inputArray = np.asarray(imageRaw)
     return inputArray
 
-def convert_intensity(nArray): //Not efficient
-    pixel =
+def singleton_convertToBlack(nArray):
+    height = nArray[0,:,:].shape[0]
+    width = nArray[:,0,:].shape[0]
+    depth = nArray[0,0,:].shape[0]
 
+    bwArray = np.zeros((imArray[:,0,:].shape[0],imArray[0,:,:].shape[0]))
+
+    for k in range(0,depth):
+        bwArray += nArray[...,k]
+
+    bwArray /= 3
+
+    return bwArray
+
+def singleton_sobel(bwArray):
+    kernel = np.zeros((3,3))
+    print(kernel.shape)
+
+    return bwArray
 
 comm = mpi.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-imageBuffer = numpy.zeros(1)
+#lineArray = np.
+imArray = load_image("earth.jpg")
+
+imageDimensions = imArray[...,0].shape
+imageWaveDepth = imArray[0,0,:].shape[0]
 
 if rank == 0:
+    imageBuffer = np.zeros((imArray[:,0,:].shape[0],imArray[0,:,:].shape[0]))
+    sobelBuffer = np.zeros((imArray[:,0,:].shape[0],imArray[0,:,:].shape[0]))
+
     if size < 2:
-        print("Operando em modo singleton")
+        print("Operating in singleton mode")
     else:
-        message = "Operando em modo paralelo com " + str(size) + " cores em uso"
+        message = "Operating in parallel mode using " + str(size) + " processing cores"
         print(message)
 
-    imArray = load_image("earth.jpg")
-
-    imageDimensions = imArray[...,0].shape
-
-    imageWaveDepth = imArray[0,0,:].shape[0]
     print(imageWaveDepth)
+    print(imageBuffer.shape)
 
-    for iterator in range(0,imageWaveDepth):
-        comm.Send(imArray[...,iterator], dest=iterator)
-        comm.Recv(imageBuffer, source=iterator)
-        #save_image(imArray[...,iterator], "earth" + str(iterator)+ ".jpg")
+    #print(np.ascontiguousarray(imArray[...,rank],dtype=np.uint8).size)
 
-else:
+    imageBuffer = singleton_convertToBlack(imArray)
 
-    comm.Recv()
+    sobelBuffer = singleton_sobel(imageBuffer)
+
+    for iterator in range(0,(imArray[:,0,:].shape[0])):
+        print("Done", iterator)
+        comm.Reduce(np.ascontiguousarray(imArray[:,iterator,rank],\
+        dtype=np.uint8),np.ascontiguousarray(imageBuffer[:,iterator],\
+        dtype=np.uint8), op=mpi.SUM, root=0)
+
+
+    save_image(imageBuffer, "earth_bw.jpg")
+
+    save_image(sobelBuffer, "earth_sobel.jpg")
