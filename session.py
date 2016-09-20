927ea5fe727f6c79c58fa754bf2d7baa4196e0c9
William Gambardella <williamcampo@gmail.com>
2016-09-20 11:22:30 -0300

    Adicionar exemplos de código para daemon UNIX
    Exemplos de código para inicialização de daemons UNIX em Python, utilizando a biblioteca python-daemon. Objetivo: facilitar a transferência de arquivos com bytecode para os hosts ativos e iniciar o MPI.

---
+++ session.py
@@ -0,0 +1,22 @@
+# *-* coding: utf-8 -*-
+
+import os
+import socket
+import logging
+
+from daemon import runner
+
+class ServerDaemon():
+
+    def __init__(self):
+        self.stdin_path = '/dev/null'
+        self.
+        
+def initialize_socket(socket_obj):
+    host = socket.gethostname()
+    port = 80
+    socket_obj.bind((host,port))
+
+
+
+main()
