a44425d7962f450f56af5a103910c7d94c393048
William Gambardella <williamcampo@gmail.com>
2016-10-16 16:52:08 -0200

    Adicionado novo método para números primos
    Métodos de números primos: peneira de Eratóstenes.

--- desafio/primes.py
+++ desafio/primes.py
@@ -12,17 +12,43 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 def vectorSize(numOfProcesses,sequenceSize, rankNumber):
-    remainder = sequenceSize%numOfProcesses
+    nop = float(numOfProcesses)
+    seqsize = float(sequenceSize)
+
+    remainder = seqsize%nop
 
     if remainder == 0:
-        return sequenceSize/numOfProcesses
+        return seqsize/nop
     else:
         if rankNumber < remainder:
-            #print math.ceil(sequenceSize/numOfProcesses)
-            return math.ceil(sequenceSize/numOfProcesses)
+            return math.ceil(seqsize/nop)
         else:
-            #print math.floor(sequenceSize/numOfProcesses)
-            return math.floor(sequenceSize/numOfProcesses)
+            return math.floor(seqsize/nop)
+
+def isPrime(workingSieve, primeFactor):
+    primeVector = []
+
+    """for position in range(0,len(workingSieve)):
+        if workingSieve[position]%2 == 0:
+            primeVector.append(0)
+        else:
+            primeFactor = 3
+            while (primeFactor*primeFactor) <= workingSieve[position]:
+                if workingSieve[position]%primeFactor == 0:
+                    primeVector.append(0)
+                else:
+                    primeFactor += 2
+            primeVector.append(workingSieve[position])
+        print primeVector"""
+
+    for pos in range(0,len(workingSieve)):
+        if workingSieve[pos] == 2:
+            primeVector.append(workingSieve[pos])
+        elif workingSieve[pos]%2 != 0:
+            primeVector.append(workingSieve[pos])
+
+    print primeVector
+    return primeVector
 
 print "I'm process number %d" % rank
 
@@ -31,54 +57,50 @@ mainSieve = None
 workerSieve = None
 workerSieveBuffer = None
 msBuffer = None
+resultBuffer = []
+resultBufferSize = 0
 displs = []
 counts = []
+pfactor = 2
 
 if rank==0:
     cmdargs = sys.argv
     nceiling = int(cmdargs[2])
-    mainSieve = np.arange(2,nceiling,dtype=np.float)
+    mainSieve = np.arange(2,nceiling+1,dtype=np.float)
 
 nceiling = comm.bcast(nceiling, root=0)
 print "I'm process number %d and the ceiling number is %d" % (rank,nceiling)
 
-
 if rank == 0:
     for x in range(0,size):
-        counts.append(vectorSize(float(size),float(nceiling-1),x))
+        counts.append(int(vectorSize(size,nceiling-1,x)))
         if x == 0:
             displs.append(0)
-        #elif x == size-1:
-        #    displs.append(displs[x-1])
         else:
-            print counts[x-1]+displs[x-1]
             displs.append(counts[x-1]+displs[x-1])
 
     counts = tuple(counts)
-    print counts
+    #print counts
 
     displs = tuple(displs)
-    print displs
+    #print displs
 
-workerSieve = np.zeros(vectorSize(float(size),float(nceiling-1),rank))
-workerSieveBuffer = np.getbuffer(np.zeros(vectorSize(float(size),float(nceiling-1),rank)))
-#print len(workerSieve)
+workerSieve = np.zeros(vectorSize(size,nceiling-1,rank))
+workerSieveBuffer = np.getbuffer(np.zeros(vectorSize(size,nceiling-1,rank)))
 
 comm.Barrier()
 
 if rank == 0:
     msBuffer = buffer(mainSieve)
 
-if rank == size-1:
-    print workerSieve
-
 comm.Scatterv([msBuffer,counts,displs,mpi.DOUBLE],workerSieveBuffer, root=0)
 
 workerSieve = np.frombuffer(workerSieveBuffer)
 
-if rank == size-1:
-    print workerSieve
-#if rank == 3:
-#    print "Process "+str(rank)+" has buffer with length "+str(len(workerSieve))
+resultBuffer = isPrime(workerSieve, 2)
 
-#comm.scatter(mainSieve,workerSieve,root=0)
+pfactorn = resultBuffer[0]
+if pfactorn == pfactor:
+    pfactorn = resultBuffer[1]
+
+pfactorn = comm.allreduce(pfactorn,op=mpi.MIN)
